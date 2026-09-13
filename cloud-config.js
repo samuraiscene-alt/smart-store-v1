941697f3d42d6574823eef860de167d796b6fd86
samuraiscene-alt <samuraiscene@gmail.com>
2026-09-13 21:15:56 +0900

    Update cloud-config.js
    Load trial notice UI

--- cloud-config.js
+++ cloud-config.js
@@ -62,4 +62,8 @@ const licenseManagerScript = document.createElement('script');
 licenseManagerScript.src = 'admin-license-manager.js?v=20260913-1';
 licenseManagerScript.defer = true;
 document.head.appendChild(licenseManagerScript);
+  const trialNoticeScript = document.createElement('script');
+trialNoticeScript.src = 'admin-trial-notice.js?v=20260913-1';
+trialNoticeScript.defer = true;
+document.head.appendChild(trialNoticeScript);
 }
